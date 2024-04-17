a4cfc80c1882d5b42514e0f006d26e195526fd7b
Kasper Korthsen <Kalletheman@live.dk>
2024-04-17 11:12:49 +0200

    Updated different things and getting ready to make connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('Test')
+import { initializeApp } from "firebase/app";
+import {
+    getFirestore, collection, getDocs
+} from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDaA5TTD1xy44cm7UpwmQQmWvNnZXKU_iE",
+    authDomain: "pushnotis-70991.firebaseapp.com",
+    projectId: "pushnotis-70991",
+    storageBucket: "pushnotis-70991.appspot.com",
+    messagingSenderId: "664596757683",
+    appId: "1:664596757683:web:f74173d4dd37fc868304c6"
+  };
+
+  // initialzing DB
+initializeApp(firebaseConfig)  
+
+  // init services
+const db = getFirestore()
+
+  // collection ref
+const colRef = collection(db, "accepted")
+
+  // get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+    console.log(snapshot.docs)
+  })
